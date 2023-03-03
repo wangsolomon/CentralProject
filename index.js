9d8138418b811081c586a299c804088176a80dce
solomon <sam1234471@gmail.com>
2023-03-04 04:56:15 +0800

    用json與圖

--- index.js
+++ index.js
@@ -2,15 +2,32 @@
 function main() {
 
   var width = 2000;
-  var scaleFactor = 10;
-  var barHeight = 20;
-  var distance = 0;
+  var height = 40000;
+  var scaleFactor = 5; //bigger times
+  var barHeight = 20; 
+  var distance = 0;  
   var flag = 0;
 
   var group = d3.select("body")
     .append("svg")
     .attr('width', width)
-    .attr('height', 40000);
+    .attr('height', height);
+
+    // 1 d3.json(json file)
+  d3.json('./sample.json').then(data => {
+    data.forEach(d => {
+      d.pedestrian_red = +d.pedestrian_red;
+      d.pedestrian_flash = +d.pedestrian_flash;
+      d.green = +d.green;
+      d.yellow = +d.yellow;
+      d.red = +d.red;
+      d.r_distance = +d.r_distance;
+
+      console.log(d.pedestrian_red);
+
+    });
+    render(data)
+  })
 
   const render = data => {
     const bar = group.selectAll("g")
@@ -20,30 +37,44 @@ function main() {
 
         var currentd;
         currentd = distance + (i * barHeight);
-        distance = d.roadDistance + distance;
-        console.log(i);
-        console.log(distance);
+        distance = d.r_distance + distance;
+        // console.log(i);
+        // console.log(distance);
 
         return 'translate(0,' + currentd + ')';
       });
 
-    //
+      
+    
     bar.append("rect").attr("width", function (d) {
-      return d.firstSecond * scaleFactor;
+      return (d.green+d.pedestrian_red+d.pedestrian_flash) * scaleFactor;
     })
-      .attr("fill", "blue")
-      .attr('height', barHeight - 1)
-      .attr('class', 'firstSecond')
-      .attr('opacity', '0.5');
+      .attr("fill", "green")
+      .attr('height', barHeight )
+      .attr('class', 'pedestrian_flash');
+    
+    
+     bar.append('svg:image')
+      .attr('x','0')
+      .attr('y',barHeight+1)
+      .attr('width',function (d) {
+        return (d.green+d.pedestrian_red+d.pedestrian_flash) * scaleFactor;
+      })
+      .attr(
+        "xlink:href", "https://1-notes.com/wp-content/uploads/2022/03/Shift%E3%82%AD%E3%83%BC%E3%82%92%E6%8A%BC%E3%81%97%E3%81%AA%E3%81%8C%E3%82%89%E6%AD%A3%E6%96%B9%E5%BD%A2%E3%82%92%E6%8F%8F%E3%81%8F.png"
+  
+      );
 
     bar.append('text')
       .attr('x', function (d) {
-        return (d.firstSecond * scaleFactor / 2);
+        return ((d.green+d.pedestrian_red+d.pedestrian_flash) * scaleFactor / 2);
       })
       .attr('y', barHeight / 2)
       .attr('dy', '.35em')
       .attr('fill', 'black')
-      .text(function (d) { return String(d.firstSecond); });
+      .text(function (d) { return String((d.green+d.pedestrian_red+d.pedestrian_flash)); });
+
+
 
 
 
@@ -52,40 +83,30 @@ function main() {
     //
 
     bar.append("rect").attr("width", function (d) {
-      if (d.secondSecond != null)
-        return d.secondSecond * scaleFactor;
+      if (d.red != null)
+        return d.red * scaleFactor;
     })
       .attr("fill", "red")
-      .attr('height', barHeight - 1).attr('class', 'secondSecond')
+      .attr('height', barHeight ).attr('class', 'green')
       .attr('transform', function (d) {
-        return 'translate(' + d.firstSecond * scaleFactor + ',0)';
+        return 'translate(' + (d.green+d.pedestrian_red+d.pedestrian_flash) * scaleFactor + ',0)';
       })
-      .attr('opacity', '0.5');
+
 
 
     bar.append('text')
       .attr('x', function (d) {
-        return ((d.secondSecond  / 2+d.firstSecond)* scaleFactor);
+        return ((d.green+d.pedestrian_red+d.pedestrian_flash)* scaleFactor);
       })
       .attr('y', barHeight / 2)
       .attr('dy', '.35em')
       .attr('fill', 'black')
-      .text(function (d) { return String(d.secondSecond); });
+      .text(function (d) { return String(d.red); });
 
 
   }
 
 
-  d3.csv('./sample.csv').then(data => {
-    data.forEach(d => {
-      d.roadDistance = +d.roadDistance;
-      d.firstSecond = +d.firstSecond;
-      d.secondSecond = +d.secondSecond;
-
-
-    });
-    render(data)
-  })
 
 
 
